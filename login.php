a3051d10cd6e15ee65eb55c8f8991224ce43c33f
SEESAndrew <78436067+SEESAndrew@users.noreply.github.com>
2021-09-13 16:50:17 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+//Starts the session and connects to the database. Code can essentially be ignored for right now.
+/*****************************************************************************************************************************************************/
+session_start();
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+ini_set('display_errors', 1);
+//Login credentials excluded on Github, making this impossible to test for the time being. Heroku should be set-up with the appropriate keys ASAP
+include ("account.php");
+$db = mysqli_connect($hostname, $username, $password, $project);
+if (mysqli_connect_errno())
+{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	exit();
+}
+print "<br>Successfully connected.<br>";
+mysqli_select_db($db, $project);
+/***************************************************************************************************************************************************/
+
+//Just reads text from the input fields. Can/Should be changed depending on what field is created.
+$username = $_GET["username"]
+$password = $_GET["password"]
+/*Checks to see if any entries in the database correspond to the input username and password. If not, it simply returns false, signifying an invalid login.
+This function can eventually be put in a separate file to remove clutter, and/or improved with a hashing algorithm and input sanitization.*/
+function valid_login($username, $password, $db)
+{
+  $statement = "select * from alpha_users where username='$username' and password='$password'";
+  ($t=mysqli_query($db, $statement)) or die(mysqli_error($db));
+  $valid = mysqli_num_rows($t);
+  if($valid==0)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+/*url must point to whatever page the user is directed to after login. A check must also be added to see if the user is an Admin or not.
+This check can be performed in a separate function if necessary*/
+if(!valid_login($username, $password, $db))
+{
+  exit("Invalid Login.")
+}
+else
+{
+  header("refresh:5 ; url=[INSERT URL HERE]")
+  exit();
+}
+?>
