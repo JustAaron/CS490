58add9dfbc24ed7a0298820f0604673a8d83db77
orioncua21 <90646834+orioncua21@users.noreply.github.com>
2021-10-13 19:08:59 -0400

    Add files via upload

---
+++ search.js
@@ -0,0 +1,80 @@
+function clearSearchBox(){
+    document.getElementById("searchField").innerHTML = "";
+    
+}
+function clearResults(){
+    document.getElementById("results").innerHTML = "";
+}
+function displayResults(results)
+{
+    var messageHTML = '<div class="res">' + results + '</div>' + '<div class="searchSpace"></div>';
+    document.getElementById("results").innerHTML += messageHTML;
+}
+
+var searchForm = document.getElementById('searchOptions');
+
+searchForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    clearResults();
+    var search = document.getElementById('searchField').value;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function(){
+        if(this.readyState == 4 && this.status == 200)
+        {
+            res=this.responseText;
+            if(res.includes('no matches'))
+            {
+                console.log("No matches found");
+                alert("No matches found");
+                clearSearchBox();
+                clearResults();
+            }
+            else
+            {
+                res = JSON.parse(res);
+                for (var i = 0; i < res.length;i++)
+                {
+                    console.log(res[i].username);
+                    if(res[i].post_id == null)
+                    {
+                        //Users
+                        console.log("Searching for a user");
+                        var user = res[i].username;
+                        displayResults(user);
+                    }
+                    else
+                    {
+                        console.log("You are searching posts");
+                        var postID = res[i].post_id;
+                        var subject = res[i].post_subject;
+                        var user = res[i].username;
+                        var string = user + " " + postID + " " + subject;
+                        displayResults(string);
+                    }
+                }
+            }
+        }
+    }
+    if(document.getElementById('postRadio').checked) 
+    {
+        xhttp.open("POST", "search_post.php", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        var sendString = 'searchpost=' + search;
+        xhttp.send(sendString);
+        console.log(search);
+        console.log("Post");
+    }
+    else if(document.getElementById('userRadio').checked) 
+    {
+        xhttp.open("POST", "search_user.php", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        var sendString = 'searchuser=' + search;
+        xhttp.send(sendString);
+        console.log(search);
+        console.log("Post");
+    }
+    else
+    {
+        alert("Please select a user or post option");
+    }
+})
---
+++ searchpage.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="Project/styles.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Page</title>
+</head>
+<body>
+    <h1>Search Page</h1>
+    <form id="searchOptions">
+        <table>
+            <tr id="radioButtons">
+                <td><input type="radio" id="postRadio" name="searchRadio"><br></td>
+                <td><label for="postRadio">Posts</label></td>
+                <td><input type="radio" id="userRadio" name="searchRadio"></td>
+                <td><label for="userRadio">Users</label></td>
+            </tr>
+            <tr id="search">
+                <td><input type="text" id="searchField" name="searchField"></td>
+                <td><button type="submit" class="searchButton">Search</button></td>
+            </tr>
+        </table>
+    </form>
+    <div id=results></div>
+    <script type="text/javascript" src="search.js"></script>
+</body>
+</html>
