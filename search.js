2dacfc0a06fc1b0542d9cd82df0cd88756f00bd9
orioncua21 <90646834+orioncua21@users.noreply.github.com>
2021-10-14 13:28:41 -0400

    Add files via upload

--- search.js
+++ search.js
@@ -1,32 +1,32 @@
-function clearSearchBox(){
+function clearSearchBox(){ //Clear the contents of the search box
     document.getElementById("searchField").innerHTML = "";
     
 }
-function clearResults(){
+function clearResults(){ //Clear the results of the previous search
     document.getElementById("results").innerHTML = "";
 }
-function displayPostResults(postID, username, subject)
+function displayPostResults(postID, username, subject) //Display the posts that match the search
 {
     var messageHTML = '<div class="res"><a href="postPage.php">' + subject + ' by '+ username + '</a></div>' + '<div class="searchSpace"></div>';
     document.getElementById("results").innerHTML += messageHTML;
 }
-function displayUserResults(username)
+function displayUserResults(username) //Display the users that match the search
 {
     var messageHTML = '<div class="res"><a href="username.php">' + username + '</a></div>' + '<div class="searchSpace"></div>';
     document.getElementById("results").innerHTML += messageHTML;
 }
 
-var searchForm = document.getElementById('searchOptions');
+var searchForm = document.getElementById('searchOptions'); 
 
-searchForm.addEventListener('submit', function(event){
+searchForm.addEventListener('submit', function(event){ //run everytime the search button is clicked
     event.preventDefault();
     clearResults();
-    var search = document.getElementById('searchField').value;
+    var search = document.getElementById('searchField').value; //What the user searched for
     var xhttp = new XMLHttpRequest();
-    xhttp.onload = function(){
+    xhttp.onload = function(){ //Runs everytime a response returns after send()
         if(this.readyState == 4 && this.status == 200)
         {
-            res=this.responseText;
+            res=this.responseText;  //The text that returns from the server
             if(res.includes('no matches'))
             {
                 console.log("No matches found");
@@ -36,11 +36,11 @@ searchForm.addEventListener('submit', function(event){
             }
             else
             {
-                res = JSON.parse(res);
+                res = JSON.parse(res); //parse the json the contains the results
                 for (var i = 0; i < res.length;i++)
                 {
                     console.log(res[i].username);
-                    if(res[i].post_id == null)
+                    if(res[i].post_id == null) 
                     {
                         //Users
                         console.log("Searching for a user");
@@ -59,7 +59,7 @@ searchForm.addEventListener('submit', function(event){
             }
         }
     }
-    if(document.getElementById('postRadio').checked) 
+    if(document.getElementById('postRadio').checked) //call search_post if the user selected posts
     {
         xhttp.open("POST", "search_post.php", true);
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -68,7 +68,7 @@ searchForm.addEventListener('submit', function(event){
         console.log(search);
         console.log("Post");
     }
-    else if(document.getElementById('userRadio').checked) 
+    else if(document.getElementById('userRadio').checked) //call search_user if the user selected users
     {
         xhttp.open("POST", "search_user.php", true);
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
