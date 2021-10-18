9be48693a41795a28332ab1166ec67f09e88b555
JustAaron <arronlee17@gmail.com>
2021-10-17 23:50:35 -0400

    Add files via upload

---
+++ get_posts.php
@@ -0,0 +1,33 @@
+<?php
+	session_start();
+	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+	ini_set('display_errors', 1);
+	require('account.php');
+	$conn = mysqli_connect($hostname, $username, $password, $project);
+	if(mysqli_connect_errno()){
+		//echo "<p>Failed to connect to MySQL: " . mysqli_connect_error(). "</p>";
+		exit();
+	}
+	function main(){
+		global $conn;
+		$post_id = $_POST['post_id'];
+		$query = 'SELECT p.post_subject, p.post_body, a.username FROM posts p, alpha_users a WHERE p.post_id=' . $post_id . ' AND p.uid=a.uid;';
+		$result = mysqli_query($conn, $query);
+		if($result && mysqli_num_rows($result) > 0){
+			$post_list = array();
+			while($row = mysqli_fetch_assoc($result)){
+				array_push($post_list, array('post_subject'=>$row['post_subject'], 'post_body'=>$row['post_body'], 'username'=>$row['username']));
+			}
+			$str = json_encode($post_list);
+			echo($str);
+		}
+		elseif(mysqli_num_rows($result) == 0){
+			echo('no comments');
+		}
+		else{
+			echo('database error');
+		}
+	}
+	main();
+	mysqli_close($conn);
+?>
