3865d45a025ffbaa7907c004e735f80947eb15f4
JustAaron <arronlee17@gmail.com>
2021-10-18 23:09:29 -0400

    Add files via upload

--- add_post.php
+++ add_post.php
@@ -1,6 +1,13 @@
 <?php
 	/*
-	Functionality: Insert into Posts table and create 
+	Functionality: Insert into Posts table and create the post file using given information. Should be used when a user creates a post. 
+	Input: 
+	In $_POST[], "post_subject" should be the subject line, "post_text" should be the body of the post.
+	In $_SESSION[], "username" should be the username of the poster, "uid" should be the uid of the poster.
+	Output: 
+	If the database insert and copying file was successful: echo "success"
+	If the database insert failed: echo "database error"
+	If the database insert succeeded but copying file failed: echo "copy file error"
 	*/
 	session_start();
 	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
@@ -15,7 +22,10 @@
 	function writeFile($max_post_id){
 		global $conn;
 		$post_username = $_SESSION['username'];
-		copy('postpage.php', $post_username . '/' . $max_post_id . '.php');
+		if(!copy('postpage.php', $post_username . '/' . $max_post_id . '.php')){
+			echo('copy file error');
+			return;
+		}
 	}
 	
 	function insertDatabase($max_post_id){
@@ -28,8 +38,10 @@
 		$add_post = 'insert into posts(post_id, post_subject, post_body, uid) values (' . $max_post_id . ',"' . $post_subject . '","' . $post_text . '",' . $post_uid . ');';
 		$result = mysqli_query($conn, $add_post);
 		if(!$result){
-			echo(mysqli_error($conn));
+			echo('database error');
+			return false;
 		}
+		return true;
 		//echo("<br> Successfully added post.");
 	}
 	
@@ -55,11 +67,11 @@
 			return;
 		}
 		//echo($max_post_id+1);
-		insertDatabase($max_post_id+1);
-		writeFile($max_post_id+1);
-		echo('<br><a href="' . $_SESSION['username'] . '/' . strval($max_post_id+1) . '.php">Created page</a>');
+		if(insertDatabase($max_post_id+1)){  // only write file if successfully inserted
+			writeFile($max_post_id+1);
+		}
+		//echo('<br><a href="' . $_SESSION['username'] . '/' . strval($max_post_id+1) . '.php">Created page</a>');
 	}
 	main();
 	mysqli_close($conn);
-	return;
 ?>
